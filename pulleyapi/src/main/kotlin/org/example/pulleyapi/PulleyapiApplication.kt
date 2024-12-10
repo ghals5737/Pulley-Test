ecbf567d2acc9de5fa3a99a2160fb29460414886
ghals5757 <ghals5737@naver.com>
2024-12-10 14:08:00 +0900

    프로젝트 세팅

---
+++ pulleyapi/src/main/kotlin/org/example/pulleyapi/PulleyapiApplication.kt
@@ -0,0 +1,11 @@
+package org.example.pulleyapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PulleyapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<PulleyapiApplication>(*args)
+}
---
+++ pulleyapi/src/test/kotlin/org/example/pulleyapi/PulleyapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.pulleyapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PulleyapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
